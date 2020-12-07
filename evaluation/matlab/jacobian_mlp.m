6eac56477b6d1194d780b05b2f49ae3ade93b5f2
Thanduriel <rojendersie@alice.de>
2020-12-07 13:52:29 +0100

    # jacobian_mlp also returns the new state

--- evaluation/matlab/jacobian_mlp.m
+++ evaluation/matlab/jacobian_mlp.m
@@ -1,15 +1,14 @@
-function J = jacobian_mlp(Ws, x)
+function [J,y] = jacobian_mlp(Ws, x)
 
-P = extend_proj(2,16);
+P = extend_proj(size(x, 2),size(Ws{1}, 1));
 y = x * P;
 J = eye(size(Ws{1}));
 
 
 for k=1:length(Ws)
     J = jacobian_layer(Ws{k}, y) * J;
-    y = y + tanh(Ws{k}*y')';
+    y = y + tanh(y * Ws{k}');
 end
 
+y = y * P';
 J = P * J * P';
-
-y = y * P'
